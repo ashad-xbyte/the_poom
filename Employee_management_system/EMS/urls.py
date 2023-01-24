de1ce7113c3bb6f16b303d6621d93e19584a2b2e
Ashad Khira <ashad.khira.xbyte@gmail.com>
2023-01-24 14:51:57 +0530

    abc

--- Employee_management_system/EMS/urls.py
+++ Employee_management_system/EMS/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('register', views.register, name="register"),
-    path('login', views.login, name="login"),
-    path('logout', views.logout, name="logout"),
+    path('user_login', views.user_login, name="user_login"),
+    path('user_logout', views.user_logout, name="user_logout"),
 ]
--- Employee_management_system/EMS/views.py
+++ Employee_management_system/EMS/views.py
@@ -2,33 +2,50 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
 
 def home(request):
     return render(request, 'auth/index.html')
 
 def register(request):
-
     if request.method == "POST":
+        username = request.POST['username']
         fname = request.POST['fname']
         lname = request.POST['lname']
         email = request.POST['email']
-        passd = request.POST['pass']
+        passd = request.POST['passd']
         cpass = request.POST['cpass']
         des = request.POST['des']
 
-        myuser = User.objects.create_user(fname, lname, email)
+        myuser = User.objects.create_user(username, email, passd)
         myuser.first_name = fname
         myuser.last_name = lname
 
         myuser.save()
 
         messages.success(request, 'Success')
-        return redirect('signin')
+        return redirect('user_login')
+
+    return render(request, 'auth/register.html')
 
 
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        passd = request.POST['passd']
+        user = authenticate(username=username, password=passd)
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, 'auth/index.html', {'fname':fname})
+        else:
+            messages.error(request, "Wrong credentials")
+            return redirect('home')
 
-def login(request):
-    return render(request, 'auth/login.html')
+    return render(request, 'auth/user_login.html')
 
-def logout(request):
-    pass    
+def user_logout(request):
+    logout(request)
+    messages.success(request, "Logged out done")
+    return redirect('home')
