1648aa61caa7bdae4b7579e00790242a8dff5110
Ashad Khira <ashad.khira.xbyte@gmail.com>
2023-01-24 12:20:06 +0530

    abc

--- Employee_management_system/EMS/views.py
+++ Employee_management_system/EMS/views.py
@@ -1,12 +1,39 @@
-from django.shortcuts import render
+import pymongo
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from pymongo import MongoClient
+
+client = pymongo.MongoClient('mongodb://localhost:27017')
+db = client['EMS']
+ems_collection = db['Ems_1']
+
 
 
 def home(request):
     return render(request, 'auth/index.html')
 
 def register(request):
-    return render(request, 'auth/register.html')
+
+    if request.method == "POST":
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        passd = request.POST['pass']
+        cpass = request.POST['cpass']
+        des = request.POST['des']
+
+        myuser = User.objects.create_user(fname, lname, email)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, 'Success')
+        return redirect('signin')
+
+
 
 def login(request):
     return render(request, 'auth/login.html')
--- Employee_management_system/Employee_management_system/settings.py
+++ Employee_management_system/Employee_management_system/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import mongoengine
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,12 +75,28 @@ WSGI_APPLICATION = 'Employee_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ems',
+#         'USER': 'root',
+#         'PASSWORD': 'xbyte123',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
+mongoengine.connect(db=db_name, host=hostname, username=username, password=pwd)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+       'default': {
+           'ENGINE': 'djongo',
+           'ENFORCE_SCHEMA': False,
+            'CLIENT': {
+                'host': 'mongodb+srv://<username>:<password>@<atlas cluster>/<myFirstDatabase>?retryWrites=true&w=majority'
+       }
+   }
+
 
 
 # Password validation
